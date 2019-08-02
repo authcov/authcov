2068973675b4a240bdf3e638ce2344bf5f321713
Evan Rolfe <evanrolfe@gmail.com>
2019-08-02 11:59:36 +0100

    Integration tests cover intrusion too

---
+++ test/integration/mpa-cookie-test.js
@@ -0,0 +1,30 @@
+const { expect } = require('chai');
+
+const UsersCrawler = require('../../lib/crawler/users-crawler.js');
+const UsersIntruder = require('../../lib/intruder/users-intruder.js');
+const ApiEndpointData = require('../../lib/data/api-endpoint-data.js');
+const PageData = require('../../lib/data/page-data.js');
+const ExampleConfig = require('../../examples/example-mpa-config.js');
+const CompareFiles = require('../utils/compare_files.js');
+
+const webAppConfig = new ExampleConfig();
+const apiEndpointData = new ApiEndpointData({webAppConfig: webAppConfig});
+const pageData = new PageData({webAppConfig: webAppConfig});
+const usersCrawler = new UsersCrawler(webAppConfig, apiEndpointData, pageData);
+const usersIntruder = new UsersIntruder(webAppConfig, apiEndpointData);
+
+// TODO: Make this tell you which apirequest is failing if it fails
+describe('UsersCrawler for MPA with cookie-based auth', () => {
+  describe('./tmp/api_endpoints.json', () => {
+    it('should save apiRequests for users: Public, evanrolfe@gmail.com, evanrolfe@onescan.io', async () => {
+      await usersCrawler.start();
+      CompareFiles.compareApiEndpointsFiles('./tmp/api_endpoints.json', './test/integration/expected_output/mpa_cookie_api_endpoints.json');
+      CompareFiles.comparePagesFiles('./tmp/pages.json', './test/integration/expected_output/mpa_cookie_pages.json');
+
+      await usersIntruder.start();
+      CompareFiles.compareApiEndpointsFiles('./tmp/api_endpoints.json', './test/integration/expected_output/mpa_cookie_api_endpoints_after_intrusion.json');
+      // Pages remain unchanged:
+      CompareFiles.comparePagesFiles('./tmp/pages.json', './test/integration/expected_output/mpa_cookie_pages.json');
+    });
+  });
+});
--- test/integration/spa-cookie-test.js
+++ test/integration/spa-cookie-test.js
@@ -1,26 +1,29 @@
 const { expect } = require('chai');
 
-const UsersCrawler = require('../../lib/crawler/users-crawler.js'); // TODO: Sort out these requires
+const UsersCrawler = require('../../lib/crawler/users-crawler.js');
+const UsersIntruder = require('../../lib/intruder/users-intruder.js');
 const ApiEndpointData = require('../../lib/data/api-endpoint-data.js');
 const PageData = require('../../lib/data/page-data.js');
 const ExampleConfig = require('../../examples/example-spa-config.js');
 const CompareFiles = require('../utils/compare_files.js');
 
-async function crawlUsers() {
-  const webAppConfig = new ExampleConfig();
-  const apiEndpointData = new ApiEndpointData({webAppConfig: webAppConfig});
-  const pageData = new PageData({webAppConfig: webAppConfig});
-
-  const usersCrawler = new UsersCrawler(webAppConfig, apiEndpointData, pageData);
-  await usersCrawler.start();
-}
+const webAppConfig = new ExampleConfig();
+const apiEndpointData = new ApiEndpointData({webAppConfig: webAppConfig});
+const pageData = new PageData({webAppConfig: webAppConfig});
+const usersCrawler = new UsersCrawler(webAppConfig, apiEndpointData, pageData);
+const usersIntruder = new UsersIntruder(webAppConfig, apiEndpointData);
 
 // TODO: Make this tell you which apirequest is failing if it fails
 describe('UsersCrawler for SPA with cookie-based auth', () => {
   describe('./tmp/api_endpoints.json', () => {
     it('should save apiRequests for users: Public, evanrolfe@gmail.com, evanrolfe@onescan.io', async () => {
-      await crawlUsers();
+      await usersCrawler.start();
       CompareFiles.compareApiEndpointsFiles('./tmp/api_endpoints.json', './test/integration/expected_output/spa_cookie_api_endpoints.json');
+      CompareFiles.comparePagesFiles('./tmp/pages.json', './test/integration/expected_output/spa_cookie_pages.json');
+
+      await usersIntruder.start();
+      CompareFiles.compareApiEndpointsFiles('./tmp/api_endpoints.json', './test/integration/expected_output/spa_cookie_api_endpoints_after_intrusion.json');
+      // Pages remain unchanged:
       CompareFiles.comparePagesFiles('./tmp/pages.json', './test/integration/expected_output/spa_cookie_pages.json');
     });
   });
