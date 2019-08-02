1c70bb72938baab484d7920f6abe1ce6098e9932
Evan Rolfe <evanrolfe@gmail.com>
2019-08-02 11:11:29 +0100

    IntruderCredentialsGrabber works with token based auth now

---
+++ examples/example-spa-token-config.js
@@ -0,0 +1,64 @@
+class Config {
+  constructor() {
+    this.users = [
+      {username: 'alice@authcov.io', password: 'password'}
+    ];
+    this.intruders = [
+      {username: 'Public', password: null},
+      {username: 'bob@authcov.io', password: 'password'}
+    ];
+    this.baseUrl = 'http://localhost:3000';
+    this.saveResponses = false;
+    this.buttonXPath = 'button';
+    this.type = 'spa';
+
+    this.authenticationType = 'token'; // cookie or token
+    this.authorisationHeaders = ['authorization'];
+    this.tokenTriggeringPage = 'http://localhost:3000/secrets';
+  }
+
+  async loginFunction(tab, username, password){
+    console.log(`Logging in as ${username}...`);
+    await tab.goto('http://localhost:3000/');
+    await tab.waitFor(1000);
+    await tab.tap('#login-link')
+
+    await tab.waitForSelector('input[name=password]');
+    await tab.waitFor(1000);
+
+    await tab.type('input[name=email]', username);
+    await tab.type('input[name=password]', password);
+
+    await tab.tap('.auth0-lock-submit')
+
+    await tab.waitFor(1000);
+    console.log('Done.');
+
+     return;
+  }
+
+  responseIsAuthorised(response) {
+    return (response.status() != 401);
+  }
+
+  ignoreApiRequest(url, method) {
+    if(url.includes('http://localhost:3000/sockjs-node')){
+      return true;
+    }
+
+    return false;
+  }
+
+  ignoreButton(outerHTML) {
+    if(outerHTML.includes('submit') || outerHTML.includes('Save')) {
+      return true;
+    }
+
+    return false;
+  }
+
+  //TODO: Add authorised checking function here
+}
+
+module.exports = Config;
+
---
+++ test/integration/intruder-credentials-grabber-test.js
@@ -0,0 +1,24 @@
+const { expect } = require('chai');
+
+const Config = require('../../examples/example-spa-token-config.js');
+const IntruderCredentialsGrabber = require('../../lib/intruder/intruder-credentials-grabber.js')
+
+const webAppConfig = new Config();
+
+// This is commented out because it relies on the example_app running on branch jwt_oauth and needs
+// to be setup so it uses Oauth login
+// TODO: Make this work without Oauth
+/*
+describe('IntruderCredentialsGrabber', () => {
+  context('for SPA with token-based auth', () => {
+    it('return the authorization header', async () => {
+      const credsGrabber = await IntruderCredentialsGrabber.init(webAppConfig);
+      const authHeaders = await credsGrabber.getAuthHeaders('evanrolfe@onescan.io', 'Password1');
+      credsGrabber.disconnect();
+
+      console.log(authHeaders);
+      expect(typeof(authHeaders.authorization)).to.equal('string');
+    });
+  });
+});
+*/
